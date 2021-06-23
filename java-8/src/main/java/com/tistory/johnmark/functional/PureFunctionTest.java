e4c2f6ad9c95034e6d453e6afeb6ee49499321d2
LoveAndCode <practice1356@gmail.com>
2021-06-23 17:37:53 +0900

    feat: add function for functional interface

--- java-8/src/main/java/com/tistory/johnmark/functional/PureFunctionTest.java
+++ java-8/src/main/java/com/tistory/johnmark/functional/PureFunctionTest.java
@@ -1,5 +1,7 @@
 package com.tistory.johnmark.functional;
 
+import java.util.function.Function;
+
 public class PureFunctionTest {
 	public static void main(String[] args) {
 		int baseNumber = 10;
@@ -8,5 +10,22 @@ public class PureFunctionTest {
 		System.out.println(pureFunction.addTen(1));
 		System.out.println(pureFunction.addTen(1));
 		System.out.println(pureFunction.addTen(1));
+
+		System.out.println("-------------------------------------");
+		Plus10 plus10 = new Plus10();
+		System.out.println(plus10.apply(1));
+		System.out.println(plus10.apply(1));
+		System.out.println(plus10.apply(1));
+
+		Function<Integer, Integer> plus20 = (number) -> number + 20;
+
+		System.out.println("-------------------------------------");
+		System.out.println(plus20.apply(10));
+		System.out.println(plus20.apply(10));
+		System.out.println("-------------------------------------");
+
+		Function<Integer, Integer> multiply2 = (i) -> i * 2;
+		System.out.println(plus10.compose(multiply2).apply(2));
+		System.out.println(plus10.andThen(multiply2).apply(2));
 	}
 }
