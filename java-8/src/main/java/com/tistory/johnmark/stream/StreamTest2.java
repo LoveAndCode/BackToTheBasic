32cf3eef79677debc5d51823816e8eaca255c41c
LoveAndCode <practice1356@gmail.com>
2021-06-26 17:37:58 +0900

    feat: add stream test code

---
+++ java-8/src/main/java/com/tistory/johnmark/stream/StreamTest2.java
@@ -0,0 +1,65 @@
+package com.tistory.johnmark.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class StreamTest2 {
+	public static void main(String[] args) {
+		List<OnlineClass> springClasses = new ArrayList<>();
+		springClasses.add(new OnlineClass(1, "spring boot", true));
+		springClasses.add(new OnlineClass(2, "spring data jpa", true));
+		springClasses.add(new OnlineClass(3, "spring mvc", false));
+		springClasses.add(new OnlineClass(4, "spring core", false));
+		springClasses.add(new OnlineClass(5, "rest api development", false));
+
+		List<OnlineClass> javaClasses = new ArrayList<>();
+		javaClasses.add(new OnlineClass(6, "The Java Test", true));
+		javaClasses.add(new OnlineClass(7, "The Java Code Development", true));
+		javaClasses.add(new OnlineClass(8, "The Java 8 to 11", false));
+
+		List<List<OnlineClass>> johnMarkEvents = new ArrayList<>();
+		johnMarkEvents.add(springClasses);
+		johnMarkEvents.add(javaClasses);
+
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("spring 으로 시작하는 수업");
+		springClasses.stream()
+			.filter(onlineClass -> onlineClass.getTitle().startsWith("spring"))
+			.forEach(System.out::println);
+		System.out.println("--------------------------------------------------------------");
+
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("close 되지 않은 수업");
+		springClasses.stream()
+			.filter(Predicate.not(OnlineClass::isClosed))
+			// .filter( onlineClass -> !onlineClass.isClosed())
+			.forEach(System.out::println);
+		System.out.println("--------------------------------------------------------------");
+
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("수업 이름만 모아서 스트림 만들기");
+		springClasses.stream()
+			.map(OnlineClass::getTitle)
+			.forEach(System.out::println);
+		System.out.println("--------------------------------------------------------------");
+
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("두 수업 목록에 둘어있는 모든 수업 아이디 출력");
+		System.out.println("--------------------------------------------------------------");
+
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("10부터 1씩 증가하는 무제한 스트림 중에 앞에 10개 빼고 최대 10개까지만");
+		System.out.println("--------------------------------------------------------------");
+
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("자바 수업 중에 Test가 들어있는 수업있는지 확인");
+		System.out.println("--------------------------------------------------------------");
+
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("스프링 수업 중에 제목에 spring이 들어간 것만 모아서 List로 만들기");
+		System.out.println("--------------------------------------------------------------");
+
+	}
+}
