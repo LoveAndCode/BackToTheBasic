799e3f6c5a909aa76f5a06db2a7053e67f11b889
LoveAndCode <practice1356@gmail.com>
2021-07-07 09:10:55 +0900

    feat: parallel sort in array

---
+++ java-8/src/main/java/com/tistory/johnmark/array/ArrayTest.java
@@ -0,0 +1,23 @@
+package com.tistory.johnmark.array;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class ArrayTest {
+	public static void main(String[] args) {
+		int size = 1500;
+		int[] numbers = new int[size];
+		Random random = new Random();
+
+		IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+		long start = System.nanoTime();
+		Arrays.sort(numbers);
+		System.out.println("Serial sorting took " + (System.nanoTime() - start));
+
+		IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+		long start2 = System.nanoTime();
+		Arrays.parallelSort(numbers);
+		System.out.println("Parallel sorting took " + (System.nanoTime() - start2));
+	}
+}
