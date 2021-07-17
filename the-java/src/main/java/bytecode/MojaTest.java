0a35385c8a2c6ae50e492ebd711ea912efb03935
LoveAndCode <practice1356@gmail.com>
2021-07-17 13:47:50 +0900

    add maven

--- the-java/src/main/java/bytecode/MojaTest.java
+++ the-java/src/main/java/bytecode/MojaTest.java
@@ -1,8 +1,23 @@
 package bytecode;
 
+import static net.bytebuddy.matcher.ElementMatchers.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FixedValue;
+
 public class MojaTest {
 	public static void main(String[] args) {
-
+		// try {
+		// 	new ByteBuddy().redefine(Moja.class).method(named("pullOut"))
+		// 				   .intercept(FixedValue.value("Rabbit!"))
+		// 				   .make().saveIn(new File(
+		// 		"C:\\Users\\JohnMark\\Desktop\\johnmark\\BackToTheBasic\\the-java\\target\\classes\\"));
+		// } catch (IOException e) {
+		// 	e.printStackTrace();
+		// }
 		System.out.println(new Moja().pullOut());
 	}
 }
