56187e446b9f1ebfbafaaac743c07aa7bcca1de8
LoveAndCode <practice1356@gmail.com>
2021-07-31 15:16:38 +0900

    feat: dynamicProxy

--- java-proxy/src/test/java/com/tistory/johnmark/javaproxy/BookServiceTest.java
+++ java-proxy/src/test/java/com/tistory/johnmark/javaproxy/BookServiceTest.java
@@ -1,9 +1,25 @@
 package com.tistory.johnmark.javaproxy;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 import org.junit.jupiter.api.Test;
 
 public class BookServiceTest {
-	BookService bookService = new BookServiceProxy(new DefaultBookService());
+	BookService bookService = (BookService)Proxy.newProxyInstance(BookService.class.getClassLoader(),
+		new Class[] {BookService.class}, new InvocationHandler() {
+			BookService bookService = new DefaultBookService();
+
+			@Override
+			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+				System.out.println("AAAAAAAAAA");
+				Object invoke = method.invoke(bookService, args);
+				System.out.println("BBBBBBBBBB");
+				return invoke;
+			}
+		}
+	);
 
 	@Test
 	public void di() {
