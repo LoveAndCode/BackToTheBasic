59872f420668beae5fc453c84280da40fef75509
LoveAndCode <practice1356@gmail.com>
2021-08-02 22:00:59 +0900

    annotation processor

---
+++ java-annotation/src/main/java/com/tistory/johnmark/App.java
@@ -0,0 +1,12 @@
+package com.tistory.johnmark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		Moja moja = new MagicMoja();
+		System.out.println(moja.pollOut());
+	}
+}
---
+++ java-annotation/src/main/java/com/tistory/johnmark/Moja.java
@@ -0,0 +1,6 @@
+package com.tistory.johnmark;
+
+@Magic
+public interface Moja {
+	String pollOut();
+}
